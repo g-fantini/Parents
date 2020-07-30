eb2d01683a27c4aff08c91e0c6c36bd852d028bc
g.fantini <gabriele.fantini@salesagility.com>
2020-07-30 15:01:09 +0200

    Person Model Definition

--- eggplant/main/models.py
+++ eggplant/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#Creating a Person model rather than customising the User one, 
+#since no authentication is mentioned or mentioned
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    #n_of_childs could be calculated by counting the 1:N relationships with other persons
+    n_of_childs = models.PositiveIntegerField()
